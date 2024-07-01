b3fbdfd8518414ce412e5563c35f07c3c6ddc575
SCOPE-MAAS <secretary@scope-maastricht.nl>
2024-07-01 23:36:22 +0200

    Users.cs

--- TeamsMigrate/Utils/Users.cs
+++ TeamsMigrate/Utils/Users.cs
@@ -356,5 +356,13 @@ namespace TeamsMigrate.Utils
                 log.Debug(httpResponseMessage.Content.ReadAsStringAsync().Result);
             }
         }
+
+        public static DateTime UnixTimeStampToDateTime(double unixTimeStamp)
+        {
+            // Unix timestamp is seconds past epoch
+            System.DateTime dtDateTime = new DateTime(1970, 1, 1, 0, 0, 0, 0, DateTimeKind.Utc);
+            dtDateTime = dtDateTime.AddSeconds(unixTimeStamp).ToLocalTime();
+            return dtDateTime;
+        }
     }
 }
